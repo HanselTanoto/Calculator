fcad8c7c0e7f8cc365fe9b701f4bde88e344d26d
Hansel Valentino Tanoto <70782721+HanselTanoto@users.noreply.github.com>
2021-03-08 13:22:11 +0700

    Update Calculator.cpp

--- Calculator.cpp
+++ Calculator.cpp
@@ -3,57 +3,62 @@
 using namespace std;
 
 // Mendefinisikan Fungsi Penjumlahan
-void add(float a, float b){
-	cout << "Result: " << a << " + " << b << " = " << a+b << endl;
+float add(float a, float b){
+	return a+b;
 }
 // Mendefinisikan Fungsi Pengurangan
-void sub(float a, float b){
-	cout << "Result: " << a << " - " << b << " = " << a-b << endl;
+float sub(float a, float b){
+	return a-b;
 }
 // Mendefinisikan Fungsi Perkalian
-void mul(float a, float b){
-	cout << "Result: " << a << " x " << b << " = " << a*b << endl;
+float mul(float a, float b){
+	return a*b;
 }
 // Mendefinisikan Fungsi Pembagian
-void div(float a, float b){
-	cout << "Result: " << a << " : " << b << " = " << a/b << endl;
+float div(float a, float b){
+	return a/b;
 }
 // Mendefinisikan Fungsi Pemangkatan
-void pow(float a, int b){
+float pow(float a, int b){
     float result = 1;
     // Loop untuk mengalikan a sampai sebanyak b kali
 	for (int i = 0; i < b; i++){
 		result *= a;
 	}
-	cout << "Result: " << a << " ^ " << b << " = " << result << endl;
+	return result;
 }
-// Mendefinisikan Fungsi Yang Akan Diintegralkan
-float f(float x){
-    return x*x + 2*x + 1;
+// Mendefinisikan Fungsi Polinomial
+float f(float x, int m, float C[]){
+    float result = 0;
+    for (int i = 0; i <= m; i++){
+		result += C[i] * pow(x,m-i);
+	}
+	return result;
 }
-// Mendefinisikan Fungsi Integral
-void intg (float a, float b, int n){
-    float dx = (b-a)/n;                 // Panjang interval : tinggi trapesium
+// Mendefinisikan Fungsi Menghitung Integral Dengan Jumlah Riemann Trapesium
+float intg (float a, float b, int n, int m, float C[]){
+    float dx = (b-a)/n;                     // Panjang interval : tinggi trapesium
     float result = 0;
     // Loop untuk menjumlahkan luas dari partisi 1 sampai partisi ke n
     for (int i = 0; i < n; i++){
-        float x1 = a + i * dx;          // x_i 
-        float x2 = a + (i + 1) * dx;    // x_(i+1)
-        float y1 = f(x1);               // y_i     : sisi sejajar ke-1 trapesium
-        float y2 = f(x2);               // y_(i+1) : sisi sejajar ke-2 trapesium
+        float x1 = a + i * dx;              // x_i 
+        float x2 = a + (i + 1) * dx;        // x_(i+1)
+        float y1 = f(x1,m,C);               // y_i     : sisi sejajar ke-1 trapesium
+        float y2 = f(x2,m,C);               // y_(i+1) : sisi sejajar ke-2 trapesium
         result += 0.5 * (y1 + y2) * dx;
     }
-    cout << "Result: " << result << endl;
+    return result;
 }
-
+// PROGRAM UTAMA
 int main(){
     cout << "----------------CALCULATOR----------------" << endl;
 	bool Continue = true;
     // Loop program kalkulator
     do{
         string op;
+        float a, b;
         // Menampilkan menu dan meminta input operator
-        cout << "Menu:\n" 
+        cout << "MENU:\n" 
                 "1. Addition       (Type 'add'  or 'Add' )\n"
                 "2. Subtraction    (Type 'sub'  or 'Sub' )\n"
                 "3. Multiplication (Type 'mul'  or 'Mul' )\n"
@@ -64,53 +69,77 @@ int main(){
 	    cin >> op;
         // Untuk operator integral
 	    if (op == "intg" || op == "Intg"){
-		    float a, b;
-		    int n;
-		    cout << "Function : f(x) = x^2 + 2x + 1" << endl;
+		    int n, m;
+			string func = {};
+            // Input untuk fungsi yang akan diintegralkan
+		    cout << "- Integral of Polynomials -" << endl;
+			cout << "  Degree of Polynomial: ";
+		    cin >> m;
+            float C[m];
+			for (int i = 0; i <= m; i++){
+				cout << "  Coefficient of x^" << m-i << ": ";
+				cin >> C[i];
+				if (m-i > 0){
+					func += to_string(C[i]) + " x^" + to_string(m-i) + " + ";
+				}
+				else{
+					func += to_string(C[i]) + " = 0";
+				}
+			}
+			cout << " Function: " << func << endl;
+            // Input untuk variabel integral
             cout << "Initial Limit: ";
 		    cin >> a;
 		    cout << "Final Limit: ";
 		    cin >> b;
 		    cout << "Number of Subinterval: ";
 		    cin >> n;
-		    intg(a,b,n);     // Memanggil fungsi intg
+		    cout << "RESULT: " << intg(a,b,n,m,C) << endl;                             // Memanggil fungsi intg
 	    }
         // Untuk operator pemangkatan
 	    else if (op == "pow" || op == "Pow"){
-		    float a; int b;
-		    cout << "Number: ";
+		    int b;
+		    cout << "Base Number: ";
 		    cin >> a;
-		    cout << "Power: ";
+		    cout << "Exponent: ";
 		    cin >> b;
-		    pow(a,b);        // Memanggil fungsi pow
+		    cout << "RESULT: " << a << " ^ " << b << " = " << pow(a,b) << endl;        // Memanggil fungsi pow
 	    }
-        // Untuk operator lainnya
-	    else{
-		    float a, b;
+        // Untuk operator penjumlahan
+		else if (op == "add" || op == "Add"){
 		    cout << "First Number: ";
 		    cin >> a;
 		    cout << "Second Number: ";
 		    cin >> b;
-            // Untuk operator penjumlahan
-		    if (op == "add" || op == "Add"){
-			    add(a,b);    // Memanggil fungsi add
-		    }
-            // Untuk operator pengurangan
-		    else if (op == "sub" || op == "Sub"){
-			    sub(a,b);    // Memanggil fungsi sub
-		    }
-            // Untuk operator perkalian
-		    else if (op == "mul" || op == "Mul"){
-			    mul(a,b);    // Memanggil fungsi mul
-		    }
-            // Untuk operator pembagian
-		    else if (op == "div" || op == "Div"){
-			    div(a,b);    // Memanggil fungsi div
-		    }
-            // Untuk operator yang tak terdefinisi
-		    else{
-			    cout << "\n-Invalid Operation-" << endl;
-		    }
+            cout << "RESULT: " << a << " + " << b << " = " << add(a,b) << endl;        // Memanggil fungsi add
+	    }
+        // Untuk operator pengurangan
+	    else if (op == "sub" || op == "Sub"){
+		    cout << "First Number: ";
+		    cin >> a;
+		    cout << "Second Number: ";
+		    cin >> b;
+            cout << "RESULT: " << a << " - " << b << " = " << sub(a,b) << endl;        // Memanggil fungsi sub
+	    }
+        // Untuk operator perkalian
+	    else if (op == "mul" || op == "Mul"){
+		    cout << "First Number: ";
+		    cin >> a;
+		    cout << "Second Number: ";
+		    cin >> b;
+            cout << "RESULT: " << a << " x " << b << " = " << mul(a,b) << endl;        // Memanggil fungsi mul
+	    }
+        // Untuk operator pembagian
+	    else if (op == "div" || op == "Div"){
+		    cout << "First Number: ";
+		    cin >> a;
+		    cout << "Second Number: ";
+		    cin >> b;
+            cout << "RESULT: " << a << " : " << b << " = " << div(a,b) << endl;        // Memanggil fungsi div
+	    }
+        // Untuk operator yang tak terdefinisi
+	    else{
+		    cout << "\n-Invalid Operation-" << endl;
 	    }
         // Meminta input untuk melanjutkan program
         char input;
